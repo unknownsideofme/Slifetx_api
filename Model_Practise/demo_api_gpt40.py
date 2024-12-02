642d2e50b855ae926c4f8b39b5163b1e4dd03ddc
Debanjan Rakshit <164716888+unknownsideofme@users.noreply.github.com>
2024-12-02 23:55:42 +0530

    Update demo_api_gpt40.py

--- Model_Practise/demo_api_gpt40.py
+++ Model_Practise/demo_api_gpt40.py
@@ -126,6 +126,6 @@ async def verify_title(request: TitleRequest):
     return json.dumps(response['answer'])
 # Run the app with uvicorn
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 5900))
     
     uvicorn.run(app, host="0.0.0.0", port=port)
